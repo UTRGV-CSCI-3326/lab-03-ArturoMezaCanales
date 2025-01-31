179308fd220218b1c317cb70051b88f93061fdc8
ArturoMezaCanales <112572320+ArturoMezaCanales@users.noreply.github.com>
2025-01-31 23:43:19 +0000

    Done

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,13 @@
+public class BankAccount {
+	public static void main(String[] args)
+    {
+        double balance = 2175.37;
+        
+        balance = ((((balance - 302.50 + 50.03) / 2) + 20.00 -1) * 2) + 1;
+    
+        System.out.printf("%.2f\n", balance);
+
+    }
+
+}
 
